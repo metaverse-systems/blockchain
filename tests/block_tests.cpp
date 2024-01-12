e5dabb706cd97e92546ae6dfbf0d77e97eb1a715
Tim Schwartz <tim@metaverse.systems>
2024-01-11 19:30:04 -0600

    Added a test suite for the block class.  This is a work in progress.

---
+++ tests/block_tests.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_all.hpp>
+#include "../src/block.hpp"
+
+TEST_CASE("Block Construction", "[block]") {
+    // Arrange
+    size_t index = 0;
+    uint64_t time = 0;
+    std::string prev_hash = "0";
+    std::string data = "";
+
+    // Act
+    block b(index, time, prev_hash, data);
+
+    // Assert
+    REQUIRE(b.index == index);
+    REQUIRE(b.timestamp == time);
+    REQUIRE(b.prevHash == prev_hash);
+    REQUIRE(b.data == data);
+    REQUIRE(!b.hash.empty()); // Hash should not be empty
+}
